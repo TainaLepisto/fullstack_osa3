40cb54c05a47254ab77f68143f0a0fd6601bdbdd
TainaLepisto <lepisto.taina@gmail.com>
2018-02-12 15:57:07 +0200

    3.20 tekeillä

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const express=require('express')
+const bodyParser=require('body-parser')
+const morgan=require('morgan')
+const cors=require('cors')
 
-const app = express()
+const Person=require('./models/person')
+
+const app=express()
 
 app.use(express.static('build'))
 
@@ -11,114 +13,124 @@ app.use(cors())
 app.use(bodyParser.json())
 
 //app.use(morgan('tiny'))
-morgan.token('reqdata', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('reqdata',function(req,res){return JSON.stringify(req.body)})
 app.use(morgan(':method :url :reqdata :status :res[content-length] - :response-time ms'))
 
-let people = [
-      {
-        "name": "Martti Tienari",
-        "number": "098765",
-        "id": 2
-      },
-      {
-        "name": "Lea Kutvonen",
-        "number": "1234",
-        "id": 4
-      },
-      {
-        "name": "lkaehrva",
-        "number": "1365",
-        "id": 10
-      },
-      {
-        "name": "ARto hellas",
-        "number": "**",
-        "id": 11
-      },
-      {
-        "name": "iolökölk",
-        "number": "t6875764",
-        "id": 12
-      },
-      {
-        "name": "qwertyu",
-        "number": "!!!",
-        "id": 13
-      }
-    ]
 
-/* tämä on frontin lisäyksen jälkeen turha
-    app.get('/', (req, res) => {
-        res.send('<h1>Hello World!</h1>')
-    })
-*/
-
-    app.get('/info', (req, res) => {
-        let paiva = new Date()
-        let kpl = people.length
-        res.send(`<p>Puhelinluettelossa tällä hetkellä ${kpl} henkilön tiedot.</p><p>${paiva}</p>`)
-    })
-
-      app.get('/api/people', (req, res) => {
-        res.json(people)
-      })
-
-
-      app.get('/api/people/:id', (req, res) => {
-        const id = Number(req.params.id)
-        const person = people.find(person => person.id === id )
-
-        if(person){
-            res.json(person)
-        } else {
-            res.status(404).end()
-        }
-
-      })
-
-      const randomId = () => {
-        return Math.round(Math.random() * 10000000)
-      }
-
-      app.post('/api/people', (req, res) => {
-        const person = req.body
-
-        if (person.name === undefined || person.name === '') {
-          return response.status(400).json({error: 'name missing'})
-        }
-        if (person.number === undefined || person.number === '') {
-          return response.status(400).json({error: 'number missing'})
-        }
-        if (people.find(p => p.name === person.name )) {
-          return response.status(400).json({error: 'name already exists'})
-        }
-
-        const personNew = {
-          name: person.name,
-          number: person.number,
-          id: randomId()
-        }
-        
-        people = people.concat(personNew)
-        res.json(personNew)
-
-      })
-
-      app.delete('/api/people/:id', (req, res) => {
-        const id = Number(req.params.id)
-        people = people.filter(person => person.id !== id)
-      
-        res.status(204).end()
-      })
-
-      /*
-      const PORT = 3001
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-      })
-*/
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.get('/api',(req,res)=>{
+  console.log("testi")
+  res.send('<h1>HelloWorld!</h1>')
 })
+
+app.get('/info',(req,res)=>{
+  let paiva=new Date()
+  Person 
+    .find({})
+    .then(people => {
+      res.send(`<p>Puhelinluettelossa tällä hetkellä ${people.length} henkilön tiedot.</p><p>${paiva}</p>`)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(404).end()
+    })
+})
+
+app.get('/api/people',(req,res)=>{
+  Person 
+    .find({})
+    .then(people => {
+      res.json(people.map(Person.format))
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(404).end()
+    })
+})
+
+app.get('/api/people/:id',(req,res)=>{
+  Person
+    .findById(req.params.id)
+    .then(person => {
+      if(person){
+        res.json(Person.format(person))
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
+})
+
+app.post('/api/people',(req,res)=>{
+  const personToBeAdded=req.body
+
+  if(personToBeAdded.name === undefined || personToBeAdded.name === ''){
+    return res.status(400).json({error:'name missing'})
+  }
+  if(personToBeAdded.number === undefined || personToBeAdded.number === ''){
+    return res.status(400).json({error:'number missing'})
+  }
+
+  Person 
+    .find({name: {'$regex': personToBeAdded.name,$options:'i'}})
+    .then(personFound => {
+      console.log(personFound)
+      if(personFound.length > 0){
+        res.status(400).json({error:'name already exists'})
+      } else {
+        const personNew=new Person({
+          name:personToBeAdded.name,
+          number:personToBeAdded.number,
+        })
+        personNew
+          .save()
+          .then(savedPerson => {
+            res.json(Person.format(savedPerson))
+          })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(404).end()
+    })
+})
+
+
+app.put('/api/people/:id', (req, res) => {
+  const person = req.body
+
+  const updatePerson = {
+    number: person.number  
+  }
+
+  Person
+    .findByIdAndUpdate(req.params.id, updatePerson, { new: true } )
+    .then(updatedPerson => {
+      res.json(Person.format(updatedPerson))
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
+})
+
+app.delete('/api/people/:id',(req,res)=>{
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
+})
+
+
+const PORT=process.env.PORT || 3001
+  app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
 
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+  }
+  
+const url = process.env.MONGODB
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+//console.log('eka parametri ', process.argv[2])
+//console.log('ja toka ', process.argv[3])
+
+if(process.argv[2] && process.argv[3]){
+    const person = new Person({
+        name: process.argv[2],
+        number: process.argv[3]
+    })
+      
+    person
+    .save()
+       .then(response => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+} else {
+    Person
+        .find({})
+        .then(result => {
+            console.log('puhelinluettelo: ')
+            result.forEach(person => {
+                console.log(person.name, ' ', person.number)
+            })
+            mongoose.connection.close()
+        })
+}
+
+
